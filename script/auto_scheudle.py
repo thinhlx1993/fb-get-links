a02873b5234d1987cbf0f93a41139256d42aa5fe
ThinhPC <thinhlx1993@gmail.com>
2021-06-13 21:30:47 +0700

    save

--- script/auto_scheudle.py
+++ script/auto_scheudle.py
@@ -29,7 +29,7 @@ def click_to(btn, region=None, waiting_time=1000):
     logger.info(f"Click to {btn}")
     start_count = 0
 
-    while start_count > waiting_time:
+    while start_count < waiting_time:
         ret = pyautogui.locateOnScreen(f"btn/{btn}", confidence=.8, region=region)
         start_count += 1
         if ret:
@@ -73,53 +73,45 @@ if __name__ == '__main__':
 
     for filename in os.listdir(dir_path):
         clipboard.copy(f"{dir_path}\\{filename}")
-        x, y = waiting_for("create_new.png")
-        pyautogui.click(x, y)
+        click_to("create_new.png")
+        click_to("upload_new_video.png")
         time.sleep(1)
-        pyautogui.click(x, y + 120)
-        time.sleep(3)
         pyautogui.hotkey("ctrl", "v")
         click_to("open.PNG")
         click_to("upload_done.png")
         click_many("close_step.png")
+
+        # paste title
         title = waiting_for("title.png")
         title_x, title_y = title
         pyautogui.click(title_x + 50, title_y)
-
         filename_without_ext = os.path.splitext(filename)[0]
         # fix title
         if '-' in filename_without_ext:
             filename_without_ext = filename_without_ext.split('-')[1]
         pyautogui.typewrite(filename_without_ext)
         click_to("next.png")
-        time.sleep(1)
-        later_btn = pyautogui.locateCenterOnScreen("btn/later.PNG", confidence=.8)
-        if later_btn:
-            pyautogui.click(later_btn)
-
+        click_to("later.PNG", waiting_time=5)
         click_to("schedule.PNG")
         schedule_x, schedule_y = waiting_for("auto_schedule.png")
 
         date_obj = datetime.fromtimestamp(date_time_obj_ts)
         # change date
-        pyautogui.click(schedule_x + 30, schedule_y)
-        time.sleep(1)
+        pyautogui.click(schedule_x + 30, schedule_y, interval=0.5)
         date = date_obj.strftime("%d/%m/%Y")
-        pyautogui.typewrite(date, interval=1)
+        pyautogui.typewrite(date, interval=0.1)
 
         # change hour
-        pyautogui.click(schedule_x + 120, schedule_y)
-        time.sleep(1)
+        pyautogui.click(schedule_x + 120, schedule_y, interval=0.5)
         hour = date_obj.strftime("%H")
-        pyautogui.typewrite(hour, interval=1)
+        pyautogui.typewrite(hour, interval=0.1)
 
         # change time
-        pyautogui.click(schedule_x + 130, schedule_y)
-        time.sleep(1)
+        pyautogui.click(schedule_x + 130, schedule_y, interval=0.5)
         minute = date_obj.strftime("%M")
-        pyautogui.typewrite(minute, interval=1)
+        pyautogui.typewrite(minute, interval=0.1)
         click_to("finish.png")
         waiting_for("done.png")
-        click_to("close_success.PNG", region=(1000,200, 300, 400))
+        click_to("close_success.PNG", region=(1000, 200, 300, 400))
         date_time_obj_ts += 3600
         os.rename(f"{dir_path}\\{filename}", f"C:\\code\\auto_schedule\\uploaded\\{filename}")
