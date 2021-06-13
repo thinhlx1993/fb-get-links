986184435ed3dca1eb810fc33cf420b64233293a
ThinhPC <thinhlx1993@gmail.com>
2021-06-13 23:22:07 +0700

    save

--- script/auto_invite_group.py
+++ script/auto_invite_group.py
@@ -10,9 +10,9 @@ from utils import logger, click_to, click_many, check_exist, waiting_for
 if __name__ == '__main__':
     number_invited = 0
     while True:
-        click_to("invite_en.PNG")
+        click_to("start_invite_group.PNG")
         waiting_for("check_box.PNG")
         number_check_box = click_many("check_box.PNG")
         number_invited += number_check_box
-        click_to("send_invite_group_en.PNG")
+        click_to("send_invite_group.PNG")
         print(f"number invited: {number_invited}")
